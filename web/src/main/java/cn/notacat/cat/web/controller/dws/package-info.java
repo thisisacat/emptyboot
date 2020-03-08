9b7c4a1d2ebffed6313f5fb02e75ca677c7cd0b9
notacat <notacat@cat.orz>
2020-03-08 18:15:10 +0800

    完善

--- web/src/main/java/cn/notacat/cat/web/controller/dws/package-info.java
+++ web/src/main/java/cn/notacat/cat/web/controller/dws/package-info.java
@@ -8,9 +8,12 @@
 package cn.notacat.cat.web.controller.dws;
 
 
-//todo 1 jackson 弄好
+//todo 1 jackson 弄好,JsonMapper 这个类弄好,项目集成jackson，排除fastjson
 //todo 2 vo层
 //todo 3 lombok
 //todo 4 thread的几个类弄哈
 //todo 5 valid几个,枚举 list 的valid
 //todo 6 项目整理成 core client的形式
+//todo 7 项目用boot配置的日志
+//todo 8 集成swag,boot admin
+//todo 9 升级tkmapper
