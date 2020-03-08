396cad3a8cd4279673277bc5906e87253a00d4bd
notacat <notacat@cat.orz>
2020-03-08 19:16:18 +0800

    lombok demo

--- web/src/main/java/cn/notacat/cat/web/controller/dws/vo/Student.java
+++ web/src/main/java/cn/notacat/cat/web/controller/dws/vo/Student.java
@@ -1,5 +1,9 @@
 package cn.notacat.cat.web.controller.dws.vo;
 
+import lombok.*;
+import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
+
 /****************************************
  * @@CREATE : 2020-03-08 6:08 下午
  * @@AUTH : NOT A CAT【NOTACAT@CAT.ORZ】
@@ -7,6 +11,15 @@ package cn.notacat.cat.web.controller.dws.vo;
  * @@VERSION :
  *
  *****************************************/
+@Data
+@EqualsAndHashCode
+@Accessors(chain = true)
+@ToString
+
+@Builder//builder模式
+@NoArgsConstructor//为了builder模式
+@AllArgsConstructor//为了builder模式
+@Slf4j//日志
 public class Student {
     private String name;
     private Integer age;
--- web/src/main/java/cn/notacat/cat/web/controller/dws/vo/Teacher.java
+++ web/src/main/java/cn/notacat/cat/web/controller/dws/vo/Teacher.java
@@ -1,5 +1,8 @@
 package cn.notacat.cat.web.controller.dws.vo;
 
+import lombok.*;
+import lombok.experimental.Accessors;
+
 /****************************************
  * @@CREATE : 2020-03-08 6:08 下午
  * @@AUTH : NOT A CAT【NOTACAT@CAT.ORZ】
@@ -7,8 +10,23 @@ package cn.notacat.cat.web.controller.dws.vo;
  * @@VERSION :
  *
  *****************************************/
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@ToString
 public class Teacher {
     private String name;
     private Integer age;
 
+
+    public static void main(String[] args) {
+//        Student s = new Student();
+//        Student s1 = new Student("a",1);
+
+//        Student s1 = new Student();
+//        System.out.println(s1.getAge());
+        Student s = Student.builder().name("1").build();
+        System.out.println(s.getName());
+        System.out.println(s.toString());
+    }
 }
