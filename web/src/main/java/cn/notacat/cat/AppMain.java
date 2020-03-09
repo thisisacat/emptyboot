7c5d611e21d66ef00c6b5de936efc3c0f1aa9545
notacat <notacat@cat.orz>
2020-03-09 11:23:07 +0800

    内嵌tomcat

--- web/src/main/java/cn/notacat/cat/AppMain.java
+++ web/src/main/java/cn/notacat/cat/AppMain.java
@@ -1,5 +1,6 @@
 package cn.notacat.cat;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -20,6 +21,10 @@ public class AppMain extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         setRegisterErrorPageFilter(false);
         return application.sources(AppMain.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppMain.class, args);
     }
 
 
